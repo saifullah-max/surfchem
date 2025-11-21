2cbba564778f2f166f6bac872c76a1f4d3ad8a37
Saad Bin Khalid <ayyansaad46@gmail.com>
2025-11-21 16:59:16 +0500

    Improved history

--- app/company-history/page.tsx
+++ app/company-history/page.tsx
@@ -1,20 +1,62 @@
-export const dynamic = 'force-dynamic';
+"use client";
 
 import { Footer } from "@/components/footer";
 import { Navigation } from "@/components/navigation";
 import { getCompanyHistory } from "@/lib/backend";
-import Head from "next/head";
+import { useEffect, useRef, useState } from "react";
 
-export default async function CompanyHistoryPage() {
-  const data = await getCompanyHistory()
+export default function CompanyHistoryPage() {
+  const [html, setHtml] = useState<string>('');
+  useEffect(() => {
+    getCompanyHistory().then(setHtml);
+    return () => { }
+  }, [])
+
   return (
     <>
-      <Head>
-        <link rel="stylesheet" href="https://www.surfchem.co.uk/assets/css/style.css" />
-      </Head>
-      <Navigation />
-      <div dangerouslySetInnerHTML={{ __html: data?.description || '' }} />;
-      <Footer />
+      <div className="bg-white">
+        <Navigation />
+        <IframeContent html={html} />
+        <Footer />
+      </div>
     </>
   );
 }
+
+const IframeContent = ({ html }: { html: string }) => {
+  const iframeRef = useRef<HTMLIFrameElement | null>(null);
+  const [height, setHeight] = useState<string | number>('200px');
+
+  const handleLoad = () => {
+    const iframe = iframeRef.current;
+    if (!iframe) return;
+    const doc = iframe.contentDocument || iframe.contentWindow?.document;
+    if (!doc) return;
+    const body = doc.body;
+    const htmlEl = doc.documentElement;
+    const newHeight = Math.max(
+      body.scrollHeight,
+      body.offsetHeight,
+      htmlEl.scrollHeight,
+      htmlEl.offsetHeight
+    );
+    setHeight(newHeight);
+  };
+
+  return (
+    <iframe
+      ref={iframeRef}
+      srcDoc={html}
+      style={{
+        width: '100%',
+        height: height,
+        border: 'none',
+        overflow: 'hidden',
+      }}
+      frameBorder="0"
+      scrolling="no"
+      onLoad={handleLoad}
+      sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+    />
+  );
+};
